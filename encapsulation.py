4d0c103c80a1396289a91cf10dd834341933fce8
sumondebnath <debnathsumon1708@gmail.com>
2024-05-15 08:35:53 +0600

    quiz add

---
+++ encapsulation.py
@@ -0,0 +1,23 @@
+
+class Bank:
+    def __init__(self, name, deposit):
+        self.name = name                        # public
+        self._branch = 'badda'                  # protected 
+        self.__balance = deposit                # private
+
+    def deposit(self, amount):
+        self.__balance += amount
+
+    def get_balance(self):
+        return self.__balance
+    
+    def withdraw(self, amount):
+        if amount < self.__balance:
+            self.__balance = self.__balance - amount
+            return amount
+        else:
+            return f'Forkia taka nai'
+
+sumon = Bank('Sumon Debnath', 100)
+sumon.deposit(300)
+print(sumon.get_balance())
